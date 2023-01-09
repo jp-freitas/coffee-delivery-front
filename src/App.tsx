ceba9364ec8a928656e6d0a63f2860cc284cb5ef
TecJP <tec.joao.freitas@gmail.com>
2023-01-09 20:02:06 -0300

    Adding the vite resolver for tsconfig paths alias and using the scroll to top component

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,15 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { CartContextProvider } from './contexts/CartContext'
 import { CoffeeContextProvider } from './contexts/CoffeeContext'
-import { AddressContextProvider } from './contexts/AddressContext'
-
+import ScrollToTop from './components/ScrollToTop'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <CoffeeContextProvider>
           <CartContextProvider>
-            <AddressContextProvider>
-              <Router />
-            </AddressContextProvider>
+            <ScrollToTop />
+            <Router />
           </CartContextProvider>
         </CoffeeContextProvider>
       </BrowserRouter>
