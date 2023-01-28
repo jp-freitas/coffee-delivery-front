e0f0ef0b0ea311e8feca51cc1e4747bb026ea206
jp-freitas <tec.joao.freitas@gmail.com>
2023-01-28 10:28:45 -0300

    Adding some values in Cart Type and rewriting the addNewProduct in cart context.

--- src/@types/cart.d.ts
+++ src/@types/cart.d.ts
@@ -1,11 +1,13 @@
+import { Address } from './address'
+
 export type Cart = {
   id: string
   products: {
     product_id: string
-    name: string
-    image: string
     quantity: number
     price: number
   }[]
   amount: number
+  address: Address
+  payment: string
 }
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -6,29 +6,30 @@ import {
   useReducer,
   useState,
 } from 'react'
-import { Address } from '@/@types/address'
-import { Coffee } from '@/@types/coffee'
+import { v4 as uuidv4 } from 'uuid'
 
+import { Address } from '@/@types/address'
 import { useCoffee } from '@/hooks/useCoffee'
+import { Cart } from '@/@types/cart'
 
 interface CartContextData {
-  cart: Coffee[]
-  address: Address
+  cart: Cart[]
+  // address: Address
   paymentMethod: string
   setPaymentMethod: Dispatch<SetStateAction<string>>
-  setAddress: Dispatch<SetStateAction<Address>>
-  cartSubTotal: number
-  delivery: number
-  total: number
+  // setAddress: Dispatch<SetStateAction<Address>>
+  // cartSubTotal: number
+  // delivery: number
+  // total: number
   addNewProduct: (id: string) => void
   handleIncreaseQuantityInCart: (id: string) => void
   handleDecreaseQuantityInCart: (id: string) => void
   handleRemoveItemFromCart: (id: string) => void
 }
 
-interface CartItemsAmount {
-  [key: string]: number
-}
+// interface CartItemsAmount {
+//   [key: string]: number
+// }
 
 interface CartContextProviderProps {
   children: ReactNode
@@ -38,67 +39,86 @@ export const CartContext = createContext<CartContextData>({} as CartContextData)
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const { coffees, resetQuantity } = useCoffee()
-  const [cart, dispatch] = useReducer((state: Coffee[], action: any) => {
+  const [cart, dispatch] = useReducer((state: Cart[], action: any) => {
     if (action.type === 'ADD_NEW_PRODUCT') {
-      return [...state, action.payload.updatedCart]
+      return [...state, action.payload.itemCart]
     }
     return state
   }, [])
-  const [address, setAddress] = useState<Address>({
-    cep: '',
-    street: '',
-    neighborhood: '',
-    complement: '',
-    city: '',
-    state: '',
-    number: '',
-  })
   const [paymentMethod, setPaymentMethod] = useState('')
-  const initialSubTotal = 0
-  const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    const newSumAmount = { ...sumAmount }
-    newSumAmount[product.id] = product.price * product.quantity
-    return newSumAmount
-  }, {} as CartItemsAmount)
-  const cartSubTotal = cart.reduce(
-    (subTotal, item) => subTotal + cartItemsAmount[item.id],
-    initialSubTotal,
-  )
-  const delivery = cart.length === 0 ? 0 : 3.5
-  const total = cartSubTotal + delivery
+  // const initialSubTotal = 0
+  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
+  //   const newSumAmount = { ...sumAmount }
+  //   newSumAmount[product.id] = product.price * product.quantity
+  //   return newSumAmount
+  // }, {} as CartItemsAmount)
+  // const cartSubTotal = cart.reduce(
+  //   (subTotal, item) => subTotal + cartItemsAmount[item.id],
+  //   initialSubTotal,
+  // )
+  // const delivery = cart.length === 0 ? 0 : 3.5
+  // const total = cartSubTotal + delivery
+
+  console.log(cart)
 
   function addNewProduct(id: string) {
     try {
-      const updatedCart = [...cart]
-      const findCoffeeInCart = updatedCart.find((coffee) => coffee.id === id)
-      const currentQuantity = findCoffeeInCart ? findCoffeeInCart.quantity : 0
-      const newQuantityCoffee = coffees.find((coffee) =>
-        coffee.id === id ? coffee.quantity : coffee,
+      const findCoffeeInCart = cart.find(
+        (coffee) => coffee.products.product_id === id,
       )
-      const newQuantityCart = currentQuantity + newQuantityCoffee!.quantity
+      const findCoffee = coffees.find((coffee) => coffee.id === id)
+
       if (findCoffeeInCart) {
-        findCoffeeInCart.quantity = newQuantityCart
+        const quantity = findCoffeeInCart.products.quantity
+        const coffeeQuantity = findCoffee!.quantity
+        findCoffeeInCart.products.quantity = quantity + coffeeQuantity
         resetQuantity(id)
       } else {
-        const findCoffee = coffees.find((coffee) => coffee.id === id)
-        if (findCoffee) {
-          const newItem = {
-            ...findCoffee,
-          }
-          updatedCart.push(newItem)
-          resetQuantity(id)
-        } else {
-          throw Error('Café não encontrado')
+        const itemCart = findCoffee && {
+          id: uuidv4(),
+          products: {
+            product_id: findCoffee.id,
+            quantity: findCoffee.quantity,
+            price: findCoffee.price,
+          },
         }
+
+        dispatch({
+          type: 'ADD_NEW_PRODUCT',
+          payload: { itemCart },
+        })
       }
-      dispatch({
-        type: 'ADD_NEW_PRODUCT',
-        payload: { updatedCart },
-      })
-      // setCart(updatedCart)
-    } catch {
-      throw Error('Erro ao adicionar produto')
+    } catch (error) {
+      throw Error('Erro ao adicionar o produto')
     }
+
+    // try {
+    //   const updatedCart = [...cart]
+    //   const findCoffeeInCart = updatedCart.find((coffee) => coffee.id === id)
+    //   const currentQuantity = findCoffeeInCart ? findCoffeeInCart.quantity : 0
+    //   const newQuantityCoffee = coffees.find((coffee) =>
+    //     coffee.id === id ? coffee.quantity : coffee,
+    //   )
+    //   const newQuantityCart = currentQuantity + newQuantityCoffee!.quantity
+    //   if (findCoffeeInCart) {
+    //     findCoffeeInCart.quantity = newQuantityCart
+    //     resetQuantity(id)
+    //   } else {
+    //     const findCoffee = coffees.find((coffee) => coffee.id === id)
+    //     if (findCoffee) {
+    //       const newItem = {
+    //         ...findCoffee,
+    //       }
+    //       updatedCart.push(newItem)
+    //       resetQuantity(id)
+    //     } else {
+    //       throw Error('Café não encontrado')
+    //     }
+    //   }
+    //   // setCart(updatedCart)
+    // } catch {
+    //   throw Error('Erro ao adicionar produto')
+    // }
   }
 
   function handleRemoveItemFromCart(id: string) {
@@ -155,13 +175,13 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     <CartContext.Provider
       value={{
         cart,
-        address,
-        setAddress,
+        // address,
+        // setAddress,
         paymentMethod,
         setPaymentMethod,
-        cartSubTotal,
-        delivery,
-        total,
+        // cartSubTotal,
+        // delivery,
+        // total,
         addNewProduct,
         handleIncreaseQuantityInCart,
         handleDecreaseQuantityInCart,
